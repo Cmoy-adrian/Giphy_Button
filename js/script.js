765c97a4c8d4defe2ff832136b91cebf68c2fd56
Cmoy07 <cmoyer07@comcast.net>
2025-10-21 06:38:21 -0400

    Made button interactive + Gif load random

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ console.log("script.js loaded");
 let endpoint = "https://api.giphy.com/v1/gifs/search?api_key=OM9f2NP9sExKxv5gsYqe6vmaDOrTZ0Jw&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
 let images = [];
 
+const gifContainer = document.querySelector("#gif-container");
+const gifButton = document.querySelector('#fetch-gif-btn')
+
+// Function to fetch Gif
 async function getGif () {
     const response = await fetch(endpoint);
     const data = await response.json();
@@ -13,7 +17,17 @@ async function getGif () {
     // Show in console for debugging
     console.log(data);
     console.log(images);
+
 }
 
-// Function for testing
-getGif ()
+// Run function
+getGif();
+
+// Listen for btn click
+gifButton.addEventListener ("click", function () {
+    // Random Gif loader
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomGif = images[randomIndex];
+    //Show the Gif
+    gifContainer.innerHTML += `<img src=${randomGif} class = "col-3 mb-3">`;
+});
